aea1b5305c98f8a7f43b60af758b82ccace059b7
MPulgarA <matiaspulgaralarcon@gmail.com>
2020-11-06 03:12:54 -0300

    Configuracion Index.js client Apollo Provider

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+//Apollo
+import client from './config/apollo';
+import {ApolloProvider} from '@apollo/client';
+
+const BeebikellApp = () =>{
+    <ApolloProvider client={client}>
+        <App/>        
+    </ApolloProvider>
+}
+
+
+AppRegistry.registerComponent(appName, () => BeebikellApp);
